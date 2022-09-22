e06e73110fd4c6f532d48d0cc9fa7acd1075f8da
Thomas Verderesi <tverderesi@gmail.com>
2022-09-22 15:15:40 -0300

    Updated imports and added new components.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import {useState} from "react";
+import { useState } from "react";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackList from "./Components/FeedbackList";
 import Header from "./Components/Header";
+import FeedbackStats from "./Components/FeedbackStats";
+import FeedbackForm from "./Components/FeedbackForm";
+
+
+
+
 function App() {
-  const [feedback,setFeedback] = useState(FeedbackData)
+  const [feedback, setFeedback] = useState(FeedbackData);
+  const deleteFeedback = id => {
+    if (window.confirm("Delete Feedback?")) {
+      setFeedback(feedback.filter(item => item.id !== id));
+    }
+  };
   return (
     <>
-    <Header />   
-    <div className="container">
-    <FeedbackList feedback={feedback}/>
-    </div>
-  </>
+      <Header />
+      <div className="container">
+        <FeedbackForm />
+        <FeedbackStats feedback={feedback}/>
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+      </div>
 
-
+    </>
   );
 }
 
