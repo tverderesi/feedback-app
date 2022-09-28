5f1439c46d20b2a9817b5c729cd024d1618e3485
Thomas Verderesi <tverderesi@gmail.com>
2022-09-28 16:06:38 -0300

    changed id type and added animations.

--- src/Components/FeedbackList.jsx
+++ src/Components/FeedbackList.jsx
@@ -1,25 +1,39 @@
-import FeedbackItem from "./FeedbackItem";
-import PropTypes from 'prop-types'
+import FeedbackItem from './FeedbackItem';
+import PropTypes from 'prop-types';
+import { motion, AnimatePresence } from 'framer-motion';
 
-
-function FeedbackList({feedback, handleDelete}) {
-    if(!feedback || feedback.length === 0) return (<p>No feedback yet.</p>)
+function FeedbackList({ feedback, handleDelete }) {
+  if (!feedback || feedback.length === 0) return <p>No feedback yet.</p>;
   return (
-    <div className="feedback-list">
-        {feedback.map((item)=> (<FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>))}
-    
+    <div className='feedback-list'>
+      <AnimatePresence>
+        {feedback.map(item => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
-  ) 
+  );
 }
 
 FeedbackList.propTypes = {
   feedback: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number.isRequired,
+      id: PropTypes.string.isRequired,
       text: PropTypes.string.isRequired,
       rating: PropTypes.number.isRequired,
     })
-  )
-}
+  ),
+};
 
-export default FeedbackList
+export default FeedbackList;
