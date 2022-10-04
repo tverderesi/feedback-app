7ec90abb0d8b824e6692181f5324dbf6d3424499
Thomas Verderesi <tverderesi@gmail.com>
2022-10-04 16:14:52 -0300

    Added Context.

--- src/Components/FeedbackStats.jsx
+++ src/Components/FeedbackStats.jsx
@@ -1,17 +1,20 @@
-import PropTypes from 'prop-types'
+import { useContext } from 'react';
+import FeedbackContext from '../Context/FeedbackContext';
 
-function FeedbackStats({feedback}) {
-    //Ratings Avg
-    let average = feedback.reduce((acc, currval) =>{return acc + currval.rating}, 0) / feedback.length
-    average = average.toFixed(1).replace(/[.,]0$/,'')
+function FeedbackStats() {
+  const { feedback } = useContext(FeedbackContext);
+  //Ratings Avg
+  let average =
+    feedback.reduce((acc, currval) => {
+      return acc + currval.rating;
+    }, 0) / feedback.length;
+  average = average.toFixed(1).replace(/[.,]0$/, '');
   return (
-    <div className="feedback-stats">
-     <h4>{feedback.length} Reviews</h4>
-     <h4>Average Rating: {isNaN(average) ? 0: average}</h4>
+    <div className='feedback-stats'>
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
     </div>
-  )
+  );
 }
 
-FeedbackStats.propTypes = {feedback: PropTypes.array.isRequired}
-
-export default FeedbackStats
+export default FeedbackStats;
