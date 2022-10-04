085436e831847712b0efcbdc4630ccc097f1c947
Thomas Verderesi <tverderesi@gmail.com>
2022-10-04 18:14:03 -0300

    Finished CRUD implementation.

--- src/Context/FeedbackContext.js
+++ src/Context/FeedbackContext.js
@@ -30,24 +30,34 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   const addFeedback = async newFeedback => {
-    const response = await fetch('/feedback', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(newFeedback),
-    });
-
-    const data = await response.json();
-    setFeedback([data, ...feedback]);
+    if (!newFeedback.rating) {
+      window.alert('No rating assigned!');
+    } else {
+      const response = await fetch('/feedback', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(newFeedback),
+      });
+      const data = await response.json();
+      setFeedback([data, ...feedback]);
+    }
   };
 
-  const deleteFeedback = id => {
+  const deleteFeedback = async id => {
     if (window.confirm('Delete Feedback?')) {
+      await fetch(`/feedback.filter/${id}`, { method: 'DELETE' });
       setFeedback(feedback.filter(item => item.id !== id));
     }
   };
 
-  const updateFeedback = (id, updateItem) => {
-    setFeedback(feedback.map(item => (item.id === id ? { ...item, ...updateItem } : item)));
+  const updateFeedback = async (id, updateItem) => {
+    const response = await fetch(`feedback/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify(updateItem),
+    });
+    const data = await response.json();
+    setFeedback(feedback.map(item => (item.id === id ? { ...item, ...data } : item)));
   };
 
   return (
